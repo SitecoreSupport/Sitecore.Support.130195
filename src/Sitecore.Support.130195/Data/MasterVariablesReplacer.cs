20a7287ad17f0a93ad644683f7d9f5e97a954db2
sc-ArtemKosohov <ark@sitecore.net>
2019-09-03 15:14:07 +0300

    Patch added

---
+++ src/Sitecore.Support.130195/Data/MasterVariablesReplacer.cs
@@ -0,0 +1,43 @@
+﻿namespace Sitecore.Support.Data
+{
+    using Sitecore;
+    using Sitecore.Collections;
+    using Sitecore.Data;
+    using Sitecore.Data.Fields;
+    using Sitecore.Data.Items;
+    using Sitecore.Diagnostics;
+    using Sitecore.Text;
+    using System;
+    using System.Collections.Generic;
+    public class MasterVariablesReplacer : Sitecore.Data.MasterVariablesReplacer
+    {
+        public MasterVariablesReplacer() : base()
+        {
+
+        }
+
+        protected override string ReplaceValues(string text, Func<string> defaultName, Func<string> defaultId, Func<string> defaultParentName, Func<string> defaultParentId)
+        {
+            if (text.Length == 0 || text.IndexOf('$') < 0)
+            {
+                return text;
+            }
+            ReplacerContext context = this.GetContext();
+            if (context != null)
+            {
+                foreach (KeyValuePair<string, string> value in context.Values)
+                {
+                    text = text.Replace(value.Key, value.Value);
+                }
+            }
+            text = ReplaceWithDefault(text, "$name", defaultName, context);
+            text = ReplaceWithDefault(text, "$id", defaultId, context);
+            text = ReplaceWithDefault(text, "$parentid", defaultParentId, context);
+            text = ReplaceWithDefault(text, "$parentname", defaultParentName, context);
+            text = ReplaceWithDefault(text, "$date", () => DateUtil.ToIsoDate(DateUtil.ToServerTime(DateTime.UtcNow).Date, false, true), context);
+            text = ReplaceWithDefault(text, "$time", () => DateUtil.IsoNowTime, context);
+            text = ReplaceWithDefault(text, "$now", () => DateUtil.IsoNow, context);
+            return text;
+        }
+    }
+}
---
+++ src/Sitecore.Support.130195/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.130195")]
+[assembly: AssemblyProduct("Sitecore.Support.130195")]
+[assembly: ComVisible(false)]
